2491d23a577396043c49d2b9165ff2b39b207d68
Algonacci <algonacci1@gmail.com>
2021-12-24 20:09:37 +0700

    updates

---
+++ 03-basic-widgets_foodist/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    // TODO: Apply Home widget
+    return MaterialApp(
+        // 3
+        theme: theme,
+        title: 'Fooderlich',
+        debugShowCheckedModeBanner: false,
+        home: const Home());
+  }
+}
