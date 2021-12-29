1286dce2f041bb5531a130443117e169784421d9
Algonacci <algonacci1@gmail.com>
2021-12-29 23:32:10 +0700

    part 4

---
+++ 04-understanding-widgets/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.light();
+    return MaterialApp(
+      theme: theme,
+      title: 'Foodist',
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+    );
+  }
+}
